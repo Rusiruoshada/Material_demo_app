46a26cea953557614a7373fbe0b79adb1527b82c
roms <rusirusamaraweera749@gmail.com>
2023-12-30 23:01:35 +0530

    customizing button

--- component/Button/Button.js
+++ component/Button/Button.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Button } from '@mui/material'
 
-export default function Button() {
+export default function ButtonField(props) {
   return (
-    <div>Button</div>
+    <Button 
+        variant={props.variant} 
+        endIcon={props.icon}  
+        type={props.type}
+        size='large'
+    >
+        {props.textField}
+    </Button>
   )
 }
