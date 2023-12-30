b95c45cb9976d2b8026750257bd0a1291bfa617e
roms <rusirusamaraweera749@gmail.com>
2023-12-30 23:01:53 +0530

    add search button to search bar

--- component/Navbar/SearchBar.js
+++ component/Navbar/SearchBar.js
@@ -1,30 +1,38 @@
 import { TextField } from '@mui/material';
 import React from 'react';
+import ButtonField from '../Button/Button';
+import { IoSearch } from 'react-icons/io5';
 
 const SearchBar = () => {
-    
-    return (
+  return (
+    <div className='flex '>
         <div className='relative flex justify-center'>
-            <TextField 
-                id="outline-search"  
-                type="search" 
-                onChange={(event) => {console.log(event.target.value)}}
+            <TextField
+                id='outline-search'
+                type='search'
+                onChange={(event) => {
+                console.log(event.target.value);
+                }}
                 sx={{
-                    width: '55%',
-                    position: 'absolute',
-                    top: '-45px',
-                    bottom: 'auto',
-                    right: 'auto',
-                    left: 'auto',
-                    backgroundColor: 'white',
-                    
+                width: '55%',
+                position: 'absolute',
+                top: '-45px',
+                bottom: 'auto',
+                right: 'auto',
+                left: 'auto',
+                backgroundColor: 'white',
                 }}
                 size='normal'
             />
-              
-            
         </div>
-    )
-}
+        <ButtonField 
+            textField='Search' 
+            variant='contained' 
+            icon= <IoSearch />
 
-export default SearchBar
+        />
+    </div>
+  );
+};
+
+export default SearchBar;
