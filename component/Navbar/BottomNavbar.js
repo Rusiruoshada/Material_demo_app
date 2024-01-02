4f5fb3349ad6e1fa3b0c6c6c4aab4e04235227ed
roms <rusirusamaraweera749@gmail.com>
2024-01-02 19:45:08 +0530

    component : modify bottom nav bar to be responsive

--- component/Navbar/BottomNavbar.js
+++ component/Navbar/BottomNavbar.js
@@ -8,6 +8,9 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import { FaShoppingCart } from 'react-icons/fa';
+import { IoMenu } from 'react-icons/io5';
+import { RxAvatar } from 'react-icons/rx';
+import { Menu, MenuItem } from '@mui/material';
 
 const pages = [
   'Home',
@@ -38,6 +41,7 @@ function ResponsiveAppBar() {
         paddingBottom: '2.5rem',
         zIndex: '0',
       }}
+      className='px-[var(--defaultPaddingSm)] md:px-[var(--defaultPaddingMd)] lg:px-[var(--defaultPadding)]'
     >
       <Container maxWidth='xl' className='px-0'>
         <Toolbar disableGutters>
@@ -48,7 +52,7 @@ function ResponsiveAppBar() {
             href='/'
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
+              display: { xs: 'block', md: 'flex' },
               fontFamily: 'monospace',
               fontWeight: 700,
               letterSpacing: '.1rem',
@@ -64,8 +68,9 @@ function ResponsiveAppBar() {
               flexGrow: 1,
               display: { xs: 'none', md: 'flex' },
               justifyContent: 'center',
-              gap: 2,
+              gap: 1,
             }}
+            className='lg:gap-1'
           >
             {pages.map((page) => (
               <Button
@@ -75,9 +80,10 @@ function ResponsiveAppBar() {
                   my: 2,
                   color: 'white',
                   display: 'block',
+                  textTransform: 'capitalize',
                   ':hover': {
                     color: '#dddddd',
-                  }
+                  },
                 }}
               >
                 {page}
@@ -85,19 +91,68 @@ function ResponsiveAppBar() {
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0, paddingX: '16px', margin: 'auto' }}>
+          <Box
+            sx={{
+              flexGrow: { md: 0, xs: 1 },
+              paddingX: '16px',
+              display: 'flex',
+              justifyContent: 'end',
+            }}
+          >
             <Tooltip title='Cart' className='relative'>
               <IconButton onClick={handleOpenCart} sx={{ p: 0 }}>
                 <FaShoppingCart className='text-white' />
                 <div className='text-xs text-white rounded-full bg-green-500 px-3 py-1 aspect-square absolute top-2 bottom-0 right-0 left-3'>
-                  <p
-                    className='flex justify-center'
-                  >
-                    0
-                  </p>
+                  <p className='flex justify-center'>0</p>
                 </div>
               </IconButton>
             </Tooltip>
+          </Box>
+
+          <Box
+            sx={{
+              flexGrow: 0,
+              display: { xs: 'flex', md: 'none' },
+              justifyContent: 'end',
+              marginX: '12px',
+              fontSize: '24px',
+            }}
+          >
+            <RxAvatar />
+          </Box>
+          <Box
+            sx={{
+              flexGrow: 0,
+              display: { xs: 'flex', md: 'none' },
+              justifyContent: 'end',
+              fontSize: '24px',
+            }}
+          >
+            <IconButton sx={{color: 'white'}} onClick={() => {}}>
+              <IoMenu />
+            </IconButton>
+            <Menu
+              sx={{ mt: '45px' }}
+              id='menu-appbar'
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={() => {}}
+            >
+              {pages.map((setting) => (
+                <MenuItem key={setting} onClick={() => {}}>
+                  <Typography textAlign='center'>{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
           </Box>
         </Toolbar>
       </Container>
