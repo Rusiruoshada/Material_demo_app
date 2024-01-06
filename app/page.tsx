f957ca35fd835800e1a1ef6aa583ccda4fde9164
roms <rusirusamaraweera749@gmail.com>
2024-01-06 23:17:14 +0530

    style : modify style

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Cart from '../component/Cart/Cart'
 
 export default function Home() {
   return (
     <>
-      
+      <header>
+        <Cart />
+      </header>
     </>
   )
 }
