983956622a03481a20cc6e5c0c146fd0a5405f5c
roms <rusirusamaraweera749@gmail.com>
2024-01-08 03:44:39 +0530

    component : edit cart

--- component/Cart/Cart.js
+++ component/Cart/Cart.js
@@ -6,7 +6,7 @@ import MobileStepper from '@mui/material/MobileStepper';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { IoIosArrowBack } from "react-icons/io";
+
 
 
 const images = [
@@ -14,25 +14,25 @@ const images = [
     label: 'San Francisco – Oakland Bay Bridge, United States',
     imgPath:
       'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-      description: 'lorem ipsem here there there is nothing but she do not understand and i think we broke up again lorem ipsem here there there is nothing but she do not understand and i think we broke up again lorem ipsem here there there is nothing but she do not understand and i think we broke up again'
+    
   },
   {
     label: 'Bird',
     imgPath:
       'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-      description: 'lorem ipsem here there there is nothing but she do not understand and i think we broke up again lorem ipsem here there there is nothing but she do not understand and i think we broke up again lorem ipsem here there there is nothing but she do not understand and i think we broke up again'
+    
   },
   {
     label: 'Bali, Indonesia',
     imgPath:
       'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
-      description: 'lorem ipsem here there there is nothing but she do not understand and i think we broke up again lorem ipsem here there there is nothing but she do not understand and i think we broke up again lorem ipsem here there there is nothing but she do not understand and i think we broke up again'
+    
   },
   {
     label: 'Goč, Serbia',
     imgPath:
       'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-      description: 'lorem ipsem here there there is nothing but she do not understand and i think we broke up again lorem ipsem here there there is nothing but she do not understand and i think we broke up again lorem ipsem here there there is nothing but she do not understand and i think we broke up again'
+    
   },
 ];
 
