91c1473d7b5ad9227434c53683c16182903d2d5e
roms <rusirusamaraweera749@gmail.com>
2024-01-09 22:59:59 +0530

    style : update position of carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Cart from '../component/Cart/Cart'
+import Carousel from '../component/CarouselImage/Carousel';
 
 export default function Home() {
   return (
     <>
-      <header>
-        <Cart />
+      <header className='lg:mt-[149px] mt-[100px]'>
+        <Carousel />
       </header>
     </>
   )
