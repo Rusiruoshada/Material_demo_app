4c4fafb1826f92b9f65769161304f664c249d305
roms <rusirusamaraweera749@gmail.com>
2024-01-11 21:02:00 +0530

    style : update style and fix bug

--- component/Cart/Cart.js
+++ component/Cart/Cart.js
@@ -8,18 +8,20 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import { MdOutlineWorkOutline } from "react-icons/md";
+import { MdOutlineWorkOutline } from 'react-icons/md';
+import { IconButton, Tooltip } from '@mui/material';
+import { FaShoppingCart } from 'react-icons/fa';
 
-export default function TemporaryDrawer({children}) {
+export default function TemporaryDrawer({ children }) {
   const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
     right: false,
   });
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
       return;
     }
 
@@ -29,7 +31,7 @@ export default function TemporaryDrawer({children}) {
   const list = (anchor) => (
     <Box
       sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-      role="presentation"
+      role='presentation'
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
@@ -38,7 +40,11 @@ export default function TemporaryDrawer({children}) {
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <MdOutlineWorkOutline /> : <MdOutlineWorkOutline />}
+                {index % 2 === 0 ? (
+                  <MdOutlineWorkOutline />
+                ) : (
+                  <MdOutlineWorkOutline />
+                )}
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
@@ -51,7 +57,11 @@ export default function TemporaryDrawer({children}) {
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <MdOutlineWorkOutline /> : <MdOutlineWorkOutline />}
+                {index % 2 === 0 ? (
+                  <MdOutlineWorkOutline />
+                ) : (
+                  <MdOutlineWorkOutline />
+                )}
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
@@ -61,11 +71,30 @@ export default function TemporaryDrawer({children}) {
     </Box>
   );
 
+
   return (
     <div>
       {['right'].map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{children}</Button>
+          <Box
+          sx={{
+            flexGrow: { md: 0, xs: 1 },
+            paddingX: '16px',
+            display: 'flex',
+            justifyContent: 'end',
+          }}
+          onClick={toggleDrawer(anchor, true)}
+          >
+            <Tooltip title='Cart' className='relative'>
+              <IconButton sx={{ p: 0 }}>
+                <FaShoppingCart className='text-white' />
+
+                <div className='text-xs text-white rounded-full bg-green-500 px-3 py-1 aspect-square absolute top-2 bottom-0 right-0 left-3'>
+                  <p className='flex justify-center'>0</p>
+                </div>
+              </IconButton>
+            </Tooltip>
+          </Box>
           <Drawer
             anchor={anchor}
             open={state[anchor]}
