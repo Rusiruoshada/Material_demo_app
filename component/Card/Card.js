08d6a3dce49191e7cf2bc8b545729bf7a8a4ee30
roms <rusirusamaraweera749@gmail.com>
2024-01-15 21:39:48 +0530

    component : modify card ui

--- component/Card/Card.js
+++ component/Card/Card.js
@@ -6,10 +6,31 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { IoIosArrowBack } from 'react-icons/io';
+import { FiPlus } from "react-icons/fi";
 import { MdDelete } from "react-icons/md";
+import { FiMinus } from "react-icons/fi";
+
 
 export default function MediaControlCard(props) {
+    const [count, setCount ] = React.useState(1);
+    const [price , setPrice ] = React.useState(props.itemPrice)
+
+
+    let countNum = count;
+    
+
+    const handleCountIncrease =()=> {
+        countNum += 1;
+        setCount(countNum);
+        setPrice(props.itemPrice * countNum)
+    };
+    const handleCountDecrease =()=> {
+        if(count === 1 ) return;
+        countNum -= 1;
+        let priceDecrease = price - props.itemPrice;
+        setCount(countNum)
+        setPrice(priceDecrease)
+    };
 
   return (
     <Card sx={{ display: 'grid', width:'95%',marginX:'auto', marginY: '10px', boxShadow:'0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 4px 10px 0 rgba(0, 0, 0, 0.19)' }}>
@@ -20,7 +41,7 @@ export default function MediaControlCard(props) {
         image={props.itemImage}
         alt={props.itemName}
       />
-        <CardContent sx={{ flex: '1 0 auto',padding:'0 8px',marginY:'auto' }}>
+        <CardContent sx={{ flex: '1 0 auto',padding:{md:'0 8px',sm:'5px', xl:'3px'},marginY:'auto' }}>
           <Typography component="div" variant="body1">
             {props.itemName}
           </Typography>
@@ -28,17 +49,27 @@ export default function MediaControlCard(props) {
             {props.itemDescription}
           </Typography>
         </CardContent>
-        <Box sx={{ display: 'flex', alignItems: 'center', pl: 0, pb: 0 }}>
-          <IconButton aria-label="previous">
-            {'rtl' === 'rtl' ? <IoIosArrowBack /> : <IoIosArrowBack />}
+
+        <Box sx={{ display: 'flex', alignItems: 'center', px: 2, pb: 0 }}>
+          <IconButton aria-label="decrease">
+             <FiMinus className='text-[16px] ' onClick={handleCountDecrease}/>
           </IconButton>
-          <IconButton aria-label="play/pause">
-            <IoIosArrowBack sx={{ height: 38, width: 38 }} />
+
+          <Typography variant="p" color="text.secondary" component="div" fontWeight={700} sx={{fontSize:'14px',marginY:'auto'}}>
+             {count}
+          </Typography>
+
+          <IconButton aria-label="increase">
+            <FiPlus className='text-[16px] ' onClick={handleCountIncrease}/>
           </IconButton>
+        </Box>
+
+          <Typography variant="body1" color="text.secondary" component="div" fontWeight={700} sx={{fontSize:'14px',marginY:'auto'}}>
+            Rs.{price}
+          </Typography>
           <IconButton aria-label="delete">
             <MdDelete className='cursor-pointer' /> 
           </IconButton>
-        </Box>
       </Box>
       
     </Card>
