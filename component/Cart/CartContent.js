745f164b7a386b0134df3755c8f8a1c273265eb5
roms <rusirusamaraweera749@gmail.com>
2024-01-25 15:39:51 +0530

    style : change style of cartItems

--- component/Cart/CartContent.js
+++ component/Cart/CartContent.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Typography, Divider, TextField } from '@mui/material';
+import { Box, Typography, Divider, TextField, Button } from '@mui/material';
 import { IoIosArrowBack } from 'react-icons/io';
 import Card from '../Card/Card';
 
@@ -13,28 +13,35 @@ const List = ({ anchor, toggleDrawer }) => {
       itemName: 'Ice cream',
       itemImage: '/1 (1).jpg',
       itemDescription: '500ml',
-      itemPrice: 800,
+      itemPrice: 2300,
     },
     {
       id:2,
       itemName: 'Ice cream',
       itemImage: '/1 (2).jpg',
       itemDescription: '500ml',
-      itemPrice: 800,
+      itemPrice: 1000,
     },
     {
       id:3,
       itemName: 'Ice cream',
       itemImage:'/1 (3).jpg',
       itemDescription: '500ml',
-      itemPrice: 800,
+      itemPrice: 1200,
     },
     {
       id:4,
       itemName: 'Ice cream',
       itemImage: '/1 (1).jpg',
       itemDescription: '500ml',
-      itemPrice: 800,
+      itemPrice: 1800,
+    },
+    {
+      id:5,
+      itemName: 'Ice cream',
+      itemImage: '/1 (1).jpg',
+      itemDescription: '500ml',
+      itemPrice: 1800,
     },
   ];
 
@@ -52,27 +59,32 @@ const List = ({ anchor, toggleDrawer }) => {
   setItems(remainItems)
   }
 
+
   return (
     <Box
       sx={{ width: {lg:450, sm:370, md:430 ,xs:320} }}
       role='presentation'
       onKeyDown={toggleDrawer(anchor, false)}
+      
     >
-      <Typography
-        textAlign='start'
-        variant='h5'
-        sx={{ display: 'flex', marginBottom: '4px', alignItems: 'center' }}
-      >
-        <IoIosArrowBack
-          className='items-center  cursor-pointer'
-          onClick={toggleDrawer(anchor, false)}
+      <div>
+        <Typography
+          textAlign='start'
+          variant='h5'
+          sx={{ display: 'flex', marginBottom: '4px', alignItems: 'center' }}
+        >
+          <IoIosArrowBack
+            className='items-center  cursor-pointer'
+            onClick={toggleDrawer(anchor, false)}
+          />
+          Cart
+        </Typography>
+        <Divider
+          sx={{ width: '90%', justifyContent: 'center', marginX: 'auto' }}
         />
-        Cart
-      </Typography>
-      <Divider
-        sx={{ width: '90%', justifyContent: 'center', marginX: 'auto' }}
-      />
-      <div className='overflow-scroll '>
+      </div>
+      
+      <div>
         {items.map(card => (
           <Card
             key={card.id}
@@ -85,43 +97,48 @@ const List = ({ anchor, toggleDrawer }) => {
           />
         ))}
       </div>
-          <Box marginX='8px'>
+
+          <Box paddingX='8px' paddingY='4px' height='10.5rem' position={'sticky'} bottom={0} bgcolor={'#fff'} >
               <Divider sx={{width:'90%', justifyContent:' center', marginX:'auto',marginY:'4px'}} />
 
               <div className='my-2 mx-4 '>
                 <div className='flex col-auto justify-between'>
-                <Typography fontWeight={450}>
-                  {checkout[0].name} :
-                </Typography>
-                <Typography>
-                  $ {checkout[0].value}
-                </Typography>
+                  <Typography fontWeight={450}>
+                    {checkout[0].name} :
+                  </Typography>
+                  <Typography>
+                    Rs. {checkout[0].value}
+                  </Typography>
                 </div>
 
                 <div className='flex col-auto justify-between'>
-                <Typography fontWeight={400} color={'gray'}>
-                  {checkout[1].name} :
-                </Typography>
-                <Typography color={'gray'}>
-                  $ {checkout[1].value}
-                </Typography>
+                  <Typography fontWeight={400} color={'gray'}>
+                    {checkout[1].name} :
+                  </Typography>
+                  <Typography color={'gray'}>
+                    Rs. {checkout[1].value}
+                  </Typography>
                 </div>
+                
+                <Divider sx={{width:'90%', justifyContent:' center', marginX:'auto',marginY:'4px'}} />
               </div>
 
-              <Divider sx={{width:'90%', justifyContent:' center', marginX:'auto',marginY:'4px'}} />
 
               <div className='my-2 mx-4 '>
-                <div className='flex col-auto justify-between'>
-                <Typography fontWeight={550}>
-                  {total.name} :
-                </Typography>
-                <Typography fontWeight={550}>
-                  $ {total.value}
-                </Typography>
+                <div className='flex col-auto justify-between mb-3'>
+                  <Typography fontWeight={550}>
+                    {total.name} :
+                  </Typography>
+                  <Typography fontWeight={550}>
+                    Rs. {total.value}
+                  </Typography>
                 </div>
+
+              <Button variant='contained' fullWidth >
+                process to Checkout
+              </Button>
+
               </div>
-
-
 
           </Box>
     </Box>
