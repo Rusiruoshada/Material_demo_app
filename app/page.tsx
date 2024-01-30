bfb1df41512cdbe877a6774a964c11ebbfe5ebfb
roms <rusirusamaraweera749@gmail.com>
2024-01-30 16:01:36 +0530

    component : modify page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,24 @@ import { TbHandClick } from 'react-icons/tb';
 import { GoThumbsup } from 'react-icons/go';
 import { Box } from '@mui/material';
 import Banner from '../component/banner/Banner';
+import upperBannerSection1 from '../public/BannerSection1A.jpg';
+import upperBannerSection2 from '../public/BannerSection2A .jpg';
+
+
+const itemData = [
+  {
+    title: 'slide1',
+    img: upperBannerSection1,
+    rows: 1,
+    cols: 1,
+  },
+  {
+    title: 'slide2',
+    img: upperBannerSection2,
+    rows: 1,
+    cols: 1,
+  },
+];
 
 export default function Home() {
   const itemsInContainer = [
@@ -24,12 +42,11 @@ export default function Home() {
       </header>
       <main>
         <div className='flex justify-center'>
-          <div className='flex justify-between'>
+          <div className='flex justify-between overflow-y-hidden overflow-x-scroll whitespace-nowrap '>
             {itemsInContainer.map((item) => (
               <Box
                 key={item.name}
-                sx={{ display: 'flex', gap: '4px', fontSize: '12px',alignItems:'center',padding:{xs:'5px',sm:'5px',md:'26px',lg:'35px' }}}
-                className=''
+                sx={{ display: 'flex', gap: '4px', fontSize: {xs:'10px',sm:'10px',md:'12px',lg:'14px'},alignItems:'center',padding:{xs:'25px 22px',sm:'25px 22px',md:'26px',lg:'35px'}}}
               >
                 {item.icon}
                 <span>{item.name}</span>
@@ -38,7 +55,7 @@ export default function Home() {
           </div>
         </div>
 
-        <Banner  />
+        <Banner bannerImages={itemData}  />
       </main>
     </>
   );
